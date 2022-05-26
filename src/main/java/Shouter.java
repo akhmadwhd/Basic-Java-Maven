092c6d2b61d438ff39530080c4236a534f86644b
annisafemiya <32125215+akhmadwhd@users.noreply.github.com>
2022-05-26 20:45:12 +0700

    Tes

--- src/main/java/Shouter.java
+++ src/main/java/Shouter.java
@@ -2,11 +2,11 @@
 public class Shouter {
 
 	public void shout(String message) {
-		
-		String shoutMessage = message.toUpperCase() + '!';
-		
+
+		String shoutMessage = message.toUpperCase() + '!'
+
 		System.out.println(shoutMessage);
-		
+
 	}
-	
+
 }
